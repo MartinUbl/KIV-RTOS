bfb8a79f843017d4e597ff961b92b98fd00c369c
MartinUbl <martinubl@seznam.cz>
2026-08-13 12:14:30 +0200

    Import SREC loader as submodule

--- sources/build.sh
+++ sources/build.sh
@@ -3,7 +3,14 @@
 mkdir -p build >/dev/null 2>&1
 cd build
 
-cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="../misc/cmake/toolchain-arm-none-eabi-rpi0.cmake" ..
+EXPANSION_BOARD=NONE
+if [ "$1" == "KIV-DPP-01" ]; then
+    EXPANSION_BOARD=KIVDPP01
+elif [ "$1" == "KIV-DPP-02" ]; then
+    EXPANSION_BOARD=KIVDPP02
+fi
+
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="../misc/cmake/toolchain-arm-none-eabi-rpi0.cmake" -DUSE_EXPANSION_BOARD=$EXPANSION_BOARD ..
 
 cmake --build . --parallel
 #make
