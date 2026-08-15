16c74b42d9829caac97df4660e7d33573807a56a
MartinUbl <martinubl@seznam.cz>
2026-08-15 10:08:53 +0200

    Import C++ and CMake extension to container, make autocomplete work

--- sources/build.sh
+++ sources/build.sh
@@ -10,7 +10,7 @@ elif [ "$1" == "KIV-DPP-02" ]; then
     EXPANSION_BOARD=KIVDPP02
 fi
 
-cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="../misc/cmake/toolchain-arm-none-eabi-rpi0.cmake" -DUSE_EXPANSION_BOARD=$EXPANSION_BOARD ..
+cmake -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="../misc/cmake/toolchain-arm-none-eabi-rpi0.cmake" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DUSE_EXPANSION_BOARD=$EXPANSION_BOARD ..
 
 cmake --build . --parallel
 #make
